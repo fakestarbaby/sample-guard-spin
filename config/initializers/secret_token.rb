264fbf5126e46713d5c34cf30080385a9c060d2b
fakestarbaby <fakestarbaby@gmail.com>
2012-01-18 18:47:19 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleGuardSpin::Application.config.secret_token = '6a05b87fc7fc5669cc2026b4d3c63cbd341611d6970822927170d80d9412faf1e43f3538378b4b8ce98220db990396b717bdbff3178073d56150d37995a03ed4'
